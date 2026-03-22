0e1245774ef0f1f026ef3014bcfdb16695c8277d
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-03-22 14:38:17 +0200

    docs: align code configuration sample

---
+++ DummyAppMock/MockerExecutionBuilderConfigurator.cs
@@ -0,0 +1,59 @@
+using DummyAppMock.Processors;
+using QaaS.Common.Generators.ConfigurationObjects.FromExternalSourceConfigurations;
+using QaaS.Common.Generators.FromExternalSourceGenerators;
+using QaaS.Framework.SDK.DataSourceObjects;
+using QaaS.Mocker;
+using QaaS.Mocker.Servers.ConfigurationObjects;
+using QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs;
+using QaaS.Mocker.Stubs.ConfigurationObjects;
+using HttpMethod = QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs.HttpMethod;
+
+namespace DummyAppMock;
+
+public sealed class MockerExecutionBuilderConfigurator : IExecutionBuilderConfigurator
+{
+    public void Configure(ExecutionBuilder executionBuilder)
+    {
+        executionBuilder
+            .CreateDataSource(new DataSourceBuilder()
+                .Named("ServerData")
+                .HookNamed(nameof(FromFileSystem))
+                .Configure(new FromFileSystemConfig
+                {
+                    DataArrangeOrder = DataArrangeOrder.AsciiAsc,
+                    FileSystem = new FileSystemConfig
+                    {
+                        Path = "ServerData"
+                    }
+                }))
+            .CreateStub(new TransactionStubBuilder()
+                .Named("ServerDataStub")
+                .HookNamed(nameof(ServerDataProcessor))
+                .AddDataSourceName("ServerData"))
+            .ReplaceServers(
+                new ServerConfig
+                {
+                    Http = new HttpServerConfig
+                    {
+                        Port = 8080,
+                        IsLocalhost = true,
+                        Endpoints =
+                        [
+                            new HttpEndpointConfig
+                            {
+                                Path = "/data",
+                                Actions =
+                                [
+                                    new HttpEndpointActionConfig
+                                    {
+                                        Name = "GetServerData",
+                                        Method = HttpMethod.Get,
+                                        TransactionStubName = "ServerDataStub"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                });
+    }
+}
