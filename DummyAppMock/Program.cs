61647c49f5fe3933f3bcfa9c35bfec786818d36a
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-03-23 17:51:34 +0200

    fix(dummyappmock): inline code configuration in program

--- DummyAppMock/Program.cs
+++ DummyAppMock/Program.cs
@@ -1,6 +1,54 @@
-var executionBuilder = new QaaS.Mocker.ExecutionBuilder();
-var configurator = new DummyAppMock.MockerExecutionBuilderConfigurator();
+using DummyAppMock.Processors;
+using QaaS.Common.Generators.ConfigurationObjects.FromExternalSourceConfigurations;
+using QaaS.Common.Generators.FromExternalSourceGenerators;
+using QaaS.Framework.SDK.DataSourceObjects;
+using QaaS.Mocker;
+using QaaS.Mocker.Servers.ConfigurationObjects;
+using QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs;
+using QaaS.Mocker.Stubs.ConfigurationObjects;
 
-configurator.Configure(executionBuilder);
+Directory.SetCurrentDirectory(AppContext.BaseDirectory);
 
-new QaaS.Mocker.MockerRunner(executionBuilder).Run();
+var executionBuilder = new ExecutionBuilder()
+    .CreateDataSource(new DataSourceBuilder()
+        .Named("ServerData")
+        .HookNamed(nameof(FromFileSystem))
+        .Configure(new FromFileSystemConfig
+        {
+            DataArrangeOrder = DataArrangeOrder.AsciiAsc,
+            FileSystem = new FileSystemConfig
+            {
+                Path = "ServerData"
+            }
+        }))
+    .CreateStub(new TransactionStubBuilder()
+        .Named("ServerDataStub")
+        .HookNamed(nameof(ServerDataProcessor))
+        .AddDataSourceName("ServerData"))
+    .ReplaceServers(
+        new ServerConfig
+        {
+            Http = new HttpServerConfig
+            {
+                Port = 8080,
+                IsLocalhost = true,
+                Endpoints =
+                [
+                    new HttpEndpointConfig
+                    {
+                        Path = "/data",
+                        Actions =
+                        [
+                            new HttpEndpointActionConfig
+                            {
+                                Name = "GetServerData",
+                                Method = QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs.HttpMethod.Get,
+                                TransactionStubName = "ServerDataStub"
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+
+new MockerRunner(executionBuilder).Run();
