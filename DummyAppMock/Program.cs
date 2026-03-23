2e0e6fae18c0fb85ad58e8499eac2208d0e2d518
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-03-23 22:36:08 +0200

    fix(dummyappmock): align code mock sample with bootstrap flow

--- DummyAppMock/Program.cs
+++ DummyAppMock/Program.cs
@@ -1,4 +1,5 @@
 using DummyAppMock.Processors;
+using System.Reflection;
 using QaaS.Common.Generators.ConfigurationObjects.FromExternalSourceConfigurations;
 using QaaS.Common.Generators.FromExternalSourceGenerators;
 using QaaS.Framework.SDK.DataSourceObjects;
@@ -7,46 +8,58 @@ using QaaS.Mocker.Servers.ConfigurationObjects;
 using QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs;
 using QaaS.Mocker.Stubs.ConfigurationObjects;
 
-var executionBuilder = new ExecutionBuilder()
-    .CreateDataSource(new DataSourceBuilder()
-        .Named("ServerData")
-        .HookNamed(nameof(FromFileSystem))
-        .Configure(new FromFileSystemConfig
-        {
-            DataArrangeOrder = DataArrangeOrder.AsciiAsc,
-            FileSystem = new FileSystemConfig
-            {
-                Path = Path.Combine(AppContext.BaseDirectory, "ServerData")
-            }
-        }))
-    .CreateStub(new TransactionStubBuilder()
-        .Named("ServerDataStub")
-        .HookNamed(nameof(ServerDataProcessor))
-        .AddDataSourceName("ServerData"))
-    .ReplaceServers(
-        new ServerConfig
-        {
-            Http = new HttpServerConfig
-            {
-                Port = 8080,
-                IsLocalhost = false,
-                Endpoints =
-                [
-                    new HttpEndpointConfig
-                    {
-                        Path = "/data",
-                        Actions =
-                        [
-                            new HttpEndpointActionConfig
-                            {
-                                Name = "GetServerData",
-                                Method = QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs.HttpMethod.Get,
-                                TransactionStubName = "ServerDataStub"
-                            }
-                        ]
-                    }
-                ]
-            }
-        });
+var runner = Bootstrap.New(args);
+var executionBuilder = GetExecutionBuilder(runner);
 
-new MockerRunner(executionBuilder).Run();
+executionBuilder.CreateDataSource(new DataSourceBuilder()
+    .Named("ServerData")
+    .HookNamed(nameof(FromFileSystem))
+    .Configure(new FromFileSystemConfig
+    {
+        DataArrangeOrder = DataArrangeOrder.AsciiAsc,
+        FileSystem = new FileSystemConfig
+        {
+            Path = Path.Combine(AppContext.BaseDirectory, "ServerData")
+        }
+    }));
+
+executionBuilder.CreateStub(new TransactionStubBuilder()
+    .Named("ServerDataStub")
+    .HookNamed(nameof(ServerDataProcessor))
+    .AddDataSourceName("ServerData"));
+
+executionBuilder.ReplaceServers(
+    new ServerConfig
+    {
+        Http = new HttpServerConfig
+        {
+            Port = 8080,
+            IsLocalhost = false,
+            Endpoints =
+            [
+                new HttpEndpointConfig
+                {
+                    Path = "/data",
+                    Actions =
+                    [
+                        new HttpEndpointActionConfig
+                        {
+                            Name = "GetServerData",
+                            Method = QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs.HttpMethod.Get,
+                            TransactionStubName = "ServerDataStub"
+                        }
+                    ]
+                }
+            ]
+        }
+    });
+
+runner.Run();
+
+static ExecutionBuilder GetExecutionBuilder(MockerRunner runner)
+{
+    return typeof(MockerRunner)
+        .GetField("_executionBuilder", BindingFlags.Instance | BindingFlags.NonPublic)?
+        .GetValue(runner) as ExecutionBuilder
+        ?? throw new InvalidOperationException("Mocker execution builder was not initialized.");
+}
