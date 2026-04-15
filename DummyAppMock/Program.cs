c7c504d801e211a3f74cebc2ac9ad1ce98f696df
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-04-15 17:17:40 +0300

    update config

--- DummyAppMock/Program.cs
+++ DummyAppMock/Program.cs
@@ -2,11 +2,16 @@ using DummyAppMock.Processors;
 using QaaS.Common.Generators.ConfigurationObjects.FromExternalSourceConfigurations;
 using QaaS.Common.Generators.FromExternalSourceGenerators;
 using QaaS.Framework.SDK.DataSourceObjects;
+using QaaS.Framework.SDK.Extensions;
 using QaaS.Mocker;
 using QaaS.Mocker.Servers.ConfigurationObjects;
 using QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs;
 using QaaS.Mocker.Stubs.ConfigurationObjects;
 using HttpMethod = QaaS.Mocker.Servers.ConfigurationObjects.HttpServerConfigs.HttpMethod;
+
+var bootstrapArguments = args.Length > 0 ? args : ["run", "mocker.qaas.yaml"];
+var runner = Bootstrap.New(bootstrapArguments);
+var executionBuilder = runner.ExecutionBuilders.AsSingle();
 
 var dataSource = new DataSourceBuilder()
     .Named("ServerData")
@@ -23,7 +28,7 @@ var dataSource = new DataSourceBuilder()
 var stub = new TransactionStubBuilder()
     .Named("ServerDataStub")
     .HookNamed(nameof(ServerDataProcessor))
-     .CreateDataSourceName("ServerData");
+    .AddDataSourceName("ServerData");
 
 var server = new ServerConfig
 {
@@ -40,9 +45,7 @@ var server = new ServerConfig
                 [
                     new HttpEndpointActionConfig
                     {
-                        Name = "GetServerData",
-                        Method = HttpMethod.Get,
-                        TransactionStubName = "ServerDataStub"
+                        Name = "GetServerData", Method = HttpMethod.Get, TransactionStubName = "ServerDataStub"
                     }
                 ]
             }
@@ -50,9 +53,9 @@ var server = new ServerConfig
     }
 };
 
-var executionBuilder = new ExecutionBuilder()
-    .CreateDataSource(dataSource)
-    .CreateStub(stub)
-    .CreateServer(server);
+executionBuilder
+    .AddDataSource(dataSource)
+    .AddStub(stub)
+    .AddServer(server);
 
-new MockerRunner(executionBuilder).Run();
+runner.Run();
